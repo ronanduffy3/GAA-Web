91a50bce2168c68b634b58716b9b86187c62f2fc
Ronan Duffy <48518654+ronanduffy3@users.noreply.github.com>
2021-01-23 16:28:05 +0000

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAarKTgvQj2eMH7YnxO0yRaIIouF0eR5ls',
+  authDomain: 'gaa-app-c0be7.firebaseapp.com',
+  databaseURL: 'https://gaa-app-c0be7.firebaseio.com',
+  projectId: 'gaa-app-c0be7',
+  storageBucket: 'gaa-app-c0be7.appspot.com',
+  messagingSenderId: '137154419494',
+  appId: '1:137154419494:web:8fb4b90790f28b6bd8bfe7',
+  measurementId: 'G-0BEVCCHW5V'
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
