3f2807f35239f3c70473c898e20b0311f4fcf4e9
ronanduffy3 <s00187127@mail.itsligo.ie>
2021-03-10 22:41:42 +0000

    Started the redesign of homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { PointsServiceService } from 'src/app/shared/services/points-service.ser
 import { AuthService } from '../../shared/services/auth.service';
 import { Table } from '../../shared/services/table';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
 
 @Component({
@@ -14,8 +15,9 @@ export class HomeComponent implements OnInit {
 
   pointsList: Table[]
   message: string;
+  user: any;
 
-  constructor(public authService: AuthService, public pointsSerice: PointsServiceService, public afAuth: AngularFireAuth) { }
+  constructor(public authService: AuthService, public pointsSerice: PointsServiceService, public afAuth: AngularFireAuth, public router: Router) { }
 
   isLoggedIn;
 
@@ -24,8 +26,9 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.checkIsLoggedIN();
 
-    let activeUser = JSON.parse(localStorage.getItem(`user`))
-    this.authService.findUserAdmin(activeUser.uid);
+    this.user = JSON.parse(localStorage.getItem(`user`))
+    this.authService.findUserAdmin(this.user.uid);
+  
 
     this.pointsSerice.getTable().subscribe({
       next: (value: Table[]) => this.pointsList = value,
@@ -39,5 +42,9 @@ export class HomeComponent implements OnInit {
   checkIsLoggedIN() {
     return this.isLoggedIn = this.authService.isLoggedIn;
   }
+
+  btnClick= function () {
+    this.router.navigate(['/list-news']);
+};
 
 }
